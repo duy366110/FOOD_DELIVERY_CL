3d0c163c421890bd5ceed99229e1f2cdfab424a3
duy366110 <duy366110@gmail.com>
2023-12-25 16:39:23 +0700

    #Commit 25/12/2023: COnfig template transaction.

--- src/components/common-component/CommonTable.vue
+++ src/components/common-component/CommonTable.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="common-table-component">
+
+        <!-- ORDER -->
         <div class="table-container" v-if="type == 'Order'">
             <table class="table-wrapper">
                 <thead>
@@ -52,6 +54,47 @@
                 </tbody>
             </table>
         </div>
+
+        <!-- TRANSACTION -->
+        <div class="table-container" v-if="type == 'Transaction'">
+            <table class="table-wrapper">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên món ăn</th>
+                        <th>Số lượng</th>
+                        <th>Đơn giá</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="(order, index) in data.orders" :key="order._id">
+                        <td>{{ index }}</td>
+                        <td>{{ order.dish.title }}</td>
+                        <td>{{ order.quantity }}</td>
+                        <td>{{ order.dish.price.$numberDecimal }} VND</td>
+                    </tr>
+
+                    <tr>
+                        <td>E-mail</td>
+                        <td colspan="3">{{ data.user.email }}</td>
+                    </tr>
+                    <tr>
+                        <td>Số điện thoại</td>
+                        <td colspan="3">{{ data.user.phone }}</td>
+                    </tr>
+                    <tr>
+                        <td>Địa chỉ</td>
+                        <td colspan="3">{{ data.user.address }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tổng đơn giá</td>
+                        <td colspan="3"> {{ total }} VND</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     </div>
 </template>
 
@@ -83,6 +126,10 @@
 </script>
 
 <style scoped>
+
+    .table-container {
+        margin-bottom: 2.5rem;   
+    }
     .table-wrapper {
         border-collapse: collapse;
         font-size: 1.8rem;
